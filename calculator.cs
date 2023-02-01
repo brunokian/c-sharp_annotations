1714dfe30d80b7eb5d1356dbe89061e9975f2b6e
brunokian <brunokian@hotmail.com>
2023-02-01 03:25:06 -0300

    calculator

---
+++ calculator.cs
@@ -0,0 +1,19 @@
+using System;
+
+namespace Calculator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        Console.Write("Enter a number")
+        int num1 = Convert.ToInt32(Console.ReadLine());
+        Console.Write("Enter another number")
+        int num2 = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine(num1 + num2)
+
+        Console.ReadLine()
+    }
+  }
+}
