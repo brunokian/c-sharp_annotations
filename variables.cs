ca5a3073db5977ee899066ccedb3f95f303110fa
brunokian <brunokian@hotmail.com>
2023-02-01 03:14:31 -0300

    variables

---
+++ variables.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace Variables
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+        string name = "Bruno"
+        int age = 26
+      Console.WriteLine("Hello, my name is " + name + " and I am " + age + "years old");    
+    }
+  }
+}
